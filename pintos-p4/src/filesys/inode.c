46b893f317a4999456cf8f86ca868d9b1b691693
yuhuahu310 <yuhuahu@usc.edu>
2020-12-04 06:58:15 -0600

    working on extensible files...

--- pintos-p4/src/filesys/inode.c
+++ pintos-p4/src/filesys/inode.c
@@ -10,6 +10,17 @@
 /* Identifies an inode. */
 #define INODE_MAGIC 0x494e4f44
 
+#define DIRECT_CNT 123
+#define INDIRECT_CNT 1
+#define DBL_INDIRECT_CNT 1
+#define SECTOR_CNT (DIRECT_CNT + INDIRECT_CNT + DBL_INDIRECT_CNT)
+
+#define PTRS_PER_SECTOR ((off_t) (BLOCK_SECTOR_SIZE / sizeof (block_sector_t)))
+#define INODE_SPAN ((DIRECT_CNT                                              \
+                     + PTRS_PER_SECTOR * INDIRECT_CNT                        \
+                     + PTRS_PER_SECTOR * PTRS_PER_SECTOR * DBL_INDIRECT_CNT) \
+                    * BLOCK_SECTOR_SIZE)
+
 /* On-disk inode.
    Must be exactly BLOCK_SECTOR_SIZE bytes long. */
 struct inode_disk
@@ -18,6 +29,9 @@ struct inode_disk
     off_t length;                       /* File size in bytes. */
     unsigned magic;                     /* Magic number. */
     uint32_t unused[125];               /* Not used. */
+
+    block_sector_t sectors[SECTOR_CNT];
+    enum inode_type type;
   };
 
 /* Returns the number of sectors to allocate for an inode SIZE
@@ -57,52 +71,54 @@ byte_to_sector (const struct inode *inode, off_t pos)
    returns the same `struct inode'. */
 static struct list open_inodes;
 
+/* Controls access to open_inodes list. */
+static struct lock open_inodes_lock;
+
+static void deallocate_inode (const struct inode *);
+
 /* Initializes the inode module. */
 void
 inode_init (void) 
 {
   list_init (&open_inodes);
+  lock_init (&open_inodes_lock);
 }
 
-/* Initializes an inode with LENGTH bytes of data and
-   writes the new inode to sector SECTOR on the file system
-   device.
-   Returns true if successful.
-   Returns false if memory or disk allocation fails. */
-bool
-inode_create (block_sector_t sector, off_t length)
+/* Initializes an inode of the given TYPE, writes the new inode
+   to sector SECTOR on the file system device, and returns the
+   inode thus created.  Returns a null pointer if unsuccessful,
+   in which case SECTOR is released in the free map. */
+struct inode *
+inode_create (block_sector_t sector, off_t length, enum inode_type type)
 {
+
   struct inode_disk *disk_inode = NULL;
-  bool success = false;
 
   ASSERT (length >= 0);
-
-  /* If this assertion fails, the inode structure is not exactly
-     one sector in size, and you should fix that. */
   ASSERT (sizeof *disk_inode == BLOCK_SECTOR_SIZE);
 
   disk_inode = calloc (1, sizeof *disk_inode);
   if (disk_inode != NULL)
+  {
+    disk_inode->length = length;
+    disk_inode->magic = INODE_MAGIC;
+    disk_inode->type = type;
+
+    block_write (fs_device, sector, disk_inode);
+
+    struct inode *mem_inode = malloc(sizeof(struct inode));
+    if (mem_inode != NULL)
     {
-      size_t sectors = bytes_to_sectors (length);
-      disk_inode->length = length;
-      disk_inode->magic = INODE_MAGIC;
-      if (free_map_allocate (sectors, &disk_inode->start)) 
-        {
-          block_write (fs_device, sector, disk_inode);
-          if (sectors > 0) 
-            {
-              static char zeros[BLOCK_SECTOR_SIZE];
-              size_t i;
-              
-              for (i = 0; i < sectors; i++) 
-                block_write (fs_device, disk_inode->start + i, zeros);
-            }
-          success = true; 
-        } 
+      mem_inode->sector = sector;
+      mem_inode->open_cnt = 0;
+      mem_inode->removed = false;
+      mem_inode->deny_write_cnt = 0;
+      mem_inode->data = *disk_inode;
       free (disk_inode);
+      return mem_inode;
     }
-  return success;
+  }
+  return NULL;
 }
 
 /* Reads an inode from SECTOR
@@ -132,12 +148,14 @@ inode_open (block_sector_t sector)
     return NULL;
 
   /* Initialize. */
+  //lock the list?
   list_push_front (&open_inodes, &inode->elem);
   inode->sector = sector;
   inode->open_cnt = 1;
   inode->deny_write_cnt = 0;
   inode->removed = false;
   block_read (fs_device, inode->sector, &inode->data);
+
   return inode;
 }
 
@@ -146,7 +164,11 @@ struct inode *
 inode_reopen (struct inode *inode)
 {
   if (inode != NULL)
+  {
+    lock_acquire (&open_inodes_lock);
     inode->open_cnt++;
+    lock_release (&open_inodes_lock);
+  }
   return inode;
 }
 
@@ -157,12 +179,20 @@ inode_get_inumber (const struct inode *inode)
   return inode->sector;
 }
 
+/* Returns the type of INODE. */
+enum inode_type
+inode_get_type (const struct inode *inode)
+{
+  return inode->data.type;
+}
+
 /* Closes INODE and writes it to disk.
    If this was the last reference to INODE, frees its memory.
    If INODE was also a removed inode, frees its blocks. */
 void
 inode_close (struct inode *inode) 
 {
+
   /* Ignore null pointer. */
   if (inode == NULL)
     return;
@@ -175,14 +205,56 @@ inode_close (struct inode *inode)
  
       /* Deallocate blocks if removed. */
       if (inode->removed) 
-        {
-          free_map_release (inode->sector, 1);
-          free_map_release (inode->data.start,
-                            bytes_to_sectors (inode->data.length)); 
-        }
+      {
+        free_map_release (inode->sector, 1);
+        deallocate_inode (inode);
+      }
 
       free (inode); 
     }
+}
+
+/* Deallocates SECTOR and anything it points to recursively.
+   LEVEL is 2 if SECTOR is doubly indirect,
+   or 1 if SECTOR is indirect,
+   or 0 if SECTOR is a data sector. */
+static void
+deallocate_recursive (block_sector_t sector, int level)
+{
+  if (level == 0) {
+    free_map_release (sector, 1);
+    return;
+  }
+  else {
+    // get direct/indirect blocks that should be deallocated
+    block_sector_t blocks[PTRS_PER_SECTOR]; 
+    block_read (fs_device, sector, blocks);
+    // deallocate those blocks
+    int i;
+    for (i = 0; i < PTRS_PER_SECTOR; i++) {
+      deallocate_recursive(blocks[i], level-1);
+    }
+    // deallocate this block?
+    free_map_release (sector, 1);
+  }
+}
+
+/* Deallocates the blocks allocated for INODE. */
+static void
+deallocate_inode (const struct inode *inode)
+{
+  int i;
+  for (i = 0; i < SECTOR_CNT; i++) {
+    if (i >= 0 && i < DIRECT_CNT) {
+      deallocate_recursive (inode->data.sectors[i], 0);
+    }
+    else if (i >= DIRECT_CNT && i < DIRECT_CNT+INDIRECT_CNT) {
+      deallocate_recursive (inode->data.sectors[i], 1);
+    }
+    else {
+      deallocate_recursive (inode->data.sectors[i], 2);
+    }
+  }
 }
 
 /* Marks INODE to be deleted when it is closed by the last caller who
--- pintos-p4/src/filesys/inode.h
+++ pintos-p4/src/filesys/inode.h
@@ -4,11 +4,18 @@
 #include <stdbool.h>
 #include "filesys/off_t.h"
 #include "devices/block.h"
+#include "threads/synch.h"
 
 struct bitmap;
 
+enum inode_type
+{
+	FILE,         /* Ordinary file. */
+	DIR           /* Directory. */
+};
+
 void inode_init (void);
-bool inode_create (block_sector_t, off_t);
+struct inode *inode_create (block_sector_t, off_t, enum inode_type);
 struct inode *inode_open (block_sector_t);
 struct inode *inode_reopen (struct inode *);
 block_sector_t inode_get_inumber (const struct inode *);
@@ -19,5 +26,6 @@ off_t inode_write_at (struct inode *, const void *, off_t size, off_t offset);
 void inode_deny_write (struct inode *);
 void inode_allow_write (struct inode *);
 off_t inode_length (const struct inode *);
+enum inode_type inode_get_type (const struct inode *);
 
 #endif /* filesys/inode.h */

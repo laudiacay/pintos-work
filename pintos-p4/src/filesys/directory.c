9696ae2bd5c97dbf675f0e900a0ece2a93730764
yuhuahu310 <yuhuahu@usc.edu>
2020-12-08 17:50:40 -0600

    dir remove small fix

--- pintos-p4/src/filesys/directory.c
+++ pintos-p4/src/filesys/directory.c
@@ -223,7 +223,7 @@ dir_is_empty (const struct dir *dir)
   struct dir_entry e;
   off_t ofs;
 
-  for (ofs = sizeof e; inode_read_at (dir->inode, &e, sizeof e, ofs) == sizeof e;
+  for (ofs = 2*sizeof e; inode_read_at (dir->inode, &e, sizeof e, ofs) == sizeof e;
        ofs += sizeof e)
   {
     if (e.in_use)
